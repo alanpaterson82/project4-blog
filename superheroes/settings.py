8974002d65724abce100c47fd0d05b808fe9197a
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-09 17:12:41 +0000

    add secret key

--- superheroes/settings.py
+++ superheroes/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8f%*7_76phi5@hoxv1r$@6)5)ep1*g)1fw+*h%_nvghd@)#s6n'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- superheroes/urls.py
+++ superheroes/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for codestar project.
+URL configuration for superheroes project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
