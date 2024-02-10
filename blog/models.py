3dc7344641b7e0c7bcced67ccd8129f6c1a011e3
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-10 11:37:12 +0000

    add the str method

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
