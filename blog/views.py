73003fe98a4982f8f5416264eb9d2875e916c527
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-10 22:07:40 +0000

    add to register and login pages

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
-    paginate_by = 3
+    paginate_by = 6
 
 def post_detail(request, slug):
     """
--- superheroes/settings.py
+++ superheroes/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alanpaterso-project4blo-h2id7bzf8pa.ws-eu108.gitpod.io',
                 '.herokuapp.com']
