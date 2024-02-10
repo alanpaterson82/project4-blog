883554a460085496c7fb9454094a264805d64b45
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-10 10:54:01 +0000

    inside migrations

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,4 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
