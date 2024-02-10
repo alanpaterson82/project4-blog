b0b03c65605620910ab88ecf66820b10486029e1
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-10 10:39:00 +0000

    add a Post model

--- superheroes/settings.py
+++ superheroes/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alanpaterso-project4blo-h2id7bzf8pa.ws-eu108.gitpod.io',
                 '.herokuapp.com']
@@ -89,6 +89,11 @@ WSGI_APPLICATION = 'superheroes.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
