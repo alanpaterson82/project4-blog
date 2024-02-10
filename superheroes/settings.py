088f32bd9d0324ffe3cbc323fc23ea1022dc3e48
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-10 13:41:56 +0000

    update admin panel

--- superheroes/settings.py
+++ superheroes/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alanpaterso-project4blo-h2id7bzf8pa.ws-eu108.gitpod.io',
                 '.herokuapp.com']
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
 ]
 
