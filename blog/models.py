5c7af977525391a4d5911f47d644cfe91f93b618
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-10 12:19:56 +0000

    generic post view

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
 
     class Meta:
         ordering = ["-created_on"]
@@ -32,3 +33,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
--- superheroes/urls.py
+++ superheroes/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as blog_views
 
 urlpatterns = [
-    path("blog/", blog_views.my_blog, name='blog'),
+    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
 ]
