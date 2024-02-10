922112861c6910799f552a8a7853ef109e000b9d
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-10 21:20:27 +0000

    add signup and login authentication

--- superheroes/urls.py
+++ superheroes/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path("background/", include("background.urls"), name="background-urls"),
     path('summernote/', include('django_summernote.urls')),
