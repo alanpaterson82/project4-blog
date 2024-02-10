bbfdf5ef845f1ecd17bd269b1de2d5d94251958a
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-10 16:20:28 +0000

    deploy with static files

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,4 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
-    paginate_by = 6
+    paginate_by = 3
