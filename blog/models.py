409934655819b40016b8a87422fedb170ce71c67
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-11 11:41:15 +0000

    enable cloudinary for images

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -10,7 +12,9 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
-    )   
+    )
+    featured_image = CloudinaryField('image', 
+    default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
