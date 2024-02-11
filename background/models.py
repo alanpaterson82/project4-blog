1cef16d596edc3c9fd71e23c625d97f9b5aeddd9
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-11 12:07:36 +0000

    edit background page with images

--- background/models.py
+++ background/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 
 class Background(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
+    profile_image = CloudinaryField('image', default='placeholder')    
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
