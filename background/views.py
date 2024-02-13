fa8c370d1c9250640d27e8b4a57202b4eb93141b
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-13 19:02:29 +0000

    amend testimonial form

--- background/views.py
+++ background/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import Background
 from .forms import TestimonialForm
 
 
 def background_info(request):
-    """
-    Renders the Background page
-    """
+    
+    if request.method == "POST":
+        testimonial_form = TestimonialForm(data=request.POST)
+        if testimonial_form.is_valid():
+            testimonial_form.save()
+            messages.add_message(request, messages.SUCCESS, "Testimonial received! Thank you for your feedback")
+
     background = Background.objects.all().order_by('-updated_on').first()
     testimonial_form = TestimonialForm()
 
